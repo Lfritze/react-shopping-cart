81c6e026ac3ae2a18010943759c97ba071da06a2
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-12-09 12:58:57 -0500

    MVP - CartContext done and imported into ShoppingCart.js and App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import data from "./data";
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
+
+// Contexts
 import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 function App() {
   const [products] = useState(data);
@@ -21,9 +24,11 @@ function App() {
     <div className="App">
       <Navigation cart={cart} />
       <ProductContext.Provider value={{ products, addItem }}>
-        <Route exact path="/" component={Products} />
-        />
-        <Route path="/cart" component={ShoppingCart} />
+        <CartContext.Provider value={{ cart }}>
+          <Route exact path="/" component={Products} />
+          />
+          <Route path="/cart" component={ShoppingCart} />
+        </CartContext.Provider>
       </ProductContext.Provider>
     </div>
   );
